b80c96333bbbbbd5b6ac00b1cf5b4a8dd54d7ef1
Abe Isleem <a.f_isleem@hotmail.com>
2026-03-24 10:27:39 +0100

    sort symlinks in symlink.sh

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,13 +4,13 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 # Create symlinks
-ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
+ln -sf "$DOTFILES_DIR/.config/opencode/opencode-notifier.json" ~/.config/opencode/opencode-notifier.json
+ln -sf "$DOTFILES_DIR/.config/opencode/opencode.json" ~/.config/opencode/opencode.json
+ln -sf "$DOTFILES_DIR/.config/opencode/tui.json" ~/.config/opencode/tui.json
+ln -sf "$DOTFILES_DIR/.config/zed/settings.json" ~/.config/zed/settings.json
 ln -sf "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
 ln -sf "$DOTFILES_DIR/.gitignore" ~/.gitignore
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
-ln -sf "$DOTFILES_DIR/.config/opencode/opencode.json" ~/.config/opencode/opencode.json
-ln -sf "$DOTFILES_DIR/.config/opencode/tui.json" ~/.config/opencode/tui.json
-ln -sf "$DOTFILES_DIR/.config/opencode/opencode-notifier.json" ~/.config/opencode/opencode-notifier.json
-ln -sf "$DOTFILES_DIR/.config/zed/settings.json" ~/.config/zed/settings.json
+ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
 
 echo "Dotfiles installed!"
