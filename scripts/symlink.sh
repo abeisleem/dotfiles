d3f95b54b7c56377b26b6177835f8471036a6712
Abe Isleem <a.f_isleem@hotmail.com>
2026-03-27 09:35:08 +0100

    symlinks entire opencode dir

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -5,12 +5,10 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 # Create symlinks
 ## Directories
-ln -s "$DOTFILES_DIR/.agents" ~/.agents
+ln -s "$DOTFILES_DIR/.agents" ~/.config/.agents/
+ln -s "$DOTFILES_DIR/.config/opencode" ~/.config/opencode
 
 ## Files
-ln -sf "$DOTFILES_DIR/.config/opencode/opencode-notifier.json" ~/.config/opencode/opencode-notifier.json
-ln -sf "$DOTFILES_DIR/.config/opencode/opencode.json" ~/.config/opencode/opencode.json
-ln -sf "$DOTFILES_DIR/.config/opencode/tui.json" ~/.config/opencode/tui.json
 ln -sf "$DOTFILES_DIR/.config/zed/settings.json" ~/.config/zed/settings.json
 ln -sf "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
 ln -sf "$DOTFILES_DIR/.gitignore" ~/.gitignore
