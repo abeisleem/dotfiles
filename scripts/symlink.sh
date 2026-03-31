6b7edd598665a6a7fae70d557fdc54fc94237ec4
Abe Isleem <a.f_isleem@hotmail.com>
2026-03-31 14:36:47 +0200

    .npmrc work

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -10,6 +10,7 @@ ln -s "$DOTFILES_DIR/.config/opencode" ~/.config
 
 ## Files
 ln -sf "$DOTFILES_DIR/.config/zed/settings.json" ~/.config/zed/settings.json
+ln -sf "$DOTFILES_DIR/.npmrc" ~/.npmrc
 ln -sf "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
 ln -sf "$DOTFILES_DIR/.gitignore" ~/.gitignore
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
