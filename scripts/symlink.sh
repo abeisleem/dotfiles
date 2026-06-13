0c1c77a3a231273435f441c30a5863d66600d737
Abe Isleem <a.f_isleem@hotmail.com>
2026-06-13 13:23:37 +0200

    chore: sort symlink lines

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -10,9 +10,9 @@ ln -s "$DOTFILES_DIR/.config/opencode" ~/.config
 
 ## Files
 ln -sf "$DOTFILES_DIR/.config/zed/settings.json" ~/.config/zed/settings.json
-ln -sf "$DOTFILES_DIR/.npmrc" ~/.npmrc
 ln -sf "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
 ln -sf "$DOTFILES_DIR/.gitignore" ~/.gitignore
+ln -sf "$DOTFILES_DIR/.npmrc" ~/.npmrc
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
 ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
 ln -sf "$DOTFILES_DIR/Brewfile" ~/Brewfile
