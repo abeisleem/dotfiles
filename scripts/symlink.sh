4d91a4988e99b9f77f3a96dfc21428ef6cdd0b53
Abe Isleem <a.f_isleem@hotmail.com>
2026-06-13 13:24:54 +0200

    add .homebrew/trust.json and symlink

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -11,6 +11,7 @@ ln -s "$DOTFILES_DIR/.config/opencode" ~/.config
 ## Files
 ln -sf "$DOTFILES_DIR/.config/zed/settings.json" ~/.config/zed/settings.json
 ln -sf "$DOTFILES_DIR/.gitconfig" ~/.gitconfig
+ln -sf "$DOTFILES_DIR/.homebrew/trust.json" ~/.homebrew/trust.json
 ln -sf "$DOTFILES_DIR/.gitignore" ~/.gitignore
 ln -sf "$DOTFILES_DIR/.npmrc" ~/.npmrc
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
